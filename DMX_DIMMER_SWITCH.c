e2745d24475ee86215fcc204fa666e14918d163a
Alister Ware <alister.ware@ntlworld.com>
2022-04-06 17:02:23 +0100

    faulty debug

--- DMX_DIMMER_SWITCH.c
+++ DMX_DIMMER_SWITCH.c
@@ -26,7 +26,6 @@
 
 #define DMX_BAUD 250000
 #define DMX_LOST_TIMEOUT 8000
-
 // map table with 255 values. Three times the same value scaled down at the end
 //uint8_t valMap[] = {90,89,89,88,88,87,87,...7,7,6,6,5,5,4,4,3,3,2,2,1,0,0};
 
@@ -38,8 +37,9 @@ volatile unsigned char phase_on_count = 0;
 volatile unsigned int live_counter = 0;
 
 volatile unsigned char val[8];
-int fade_delay = 0;
-int fade_pos =0;
+volatile int fade_delay = 0;
+volatile int fade_pos =0;
+uint8_t fade_dir = 1;
 
 #define SPI_DDR                 DDRB
 #define SPI_PORT                PORTB
@@ -50,6 +50,44 @@ int fade_pos =0;
 #define CS_595_HI()				PORTB &= ~(1<<CS_595)
 #define CS_595_LO()				PORTB |= (1<<CS_595)
 
+void out_ext (unsigned char value);
+
+ISR (TIMER2_COMPA_vect)
+{
+	unsigned char out = 0;
+	unsigned char tmp = 0;
+	phase_on_count++;
+
+	if(phase_on_count > 240){
+		phase_on_count = 0;
+	}
+
+	for(tmp = 0;tmp <8;tmp++){
+		if(phase_on_count>val[tmp]) out |= (1<<tmp);
+//		if(phase_on_count>65) out |= (1<<tmp);
+	}
+	out_ext(out);
+
+	if(dmx_lost<DMX_LOST_TIMEOUT){
+		dmx_lost++;
+	}
+}
+
+ ISR (TIMER1_COMPA_vect)
+ {
+	if (fade_dir){
+		fade_pos++;
+		if (fade_pos ==0){
+			fade_dir = 0;
+		}
+	} else {
+		fade_pos--;
+		if (fade_pos ==0xff){
+			fade_dir = 1;
+		}
+	}
+}
+
 //############################################################################
 //Receiving routine for DMX
 ISR (USART_RX_vect){
@@ -57,7 +95,7 @@ ISR (USART_RX_vect){
 	static unsigned char dmx_valid = 0;
 	unsigned char tmp = 0;
 	tmp =  UDR0;
-	LED_ON
+//                                                                                                                                                                                                                                                                                 	LED_ON
 	live_counter=0;
 	if(UCSR0A&(1<<FE0)){
 		if(dmx_channel_rx_count > 1){
@@ -110,27 +148,6 @@ static inline void spi_init(void){
 
 //############################################################################
 //Time is counted here (Tick 100µs
-ISR (TIMER2_COMPA_vect)
-{
-	unsigned char out = 0;
-	unsigned char tmp = 0;
-	phase_on_count++;
-
-	if(phase_on_count > 240){
-		phase_on_count = 0;
-	}
-
-	for(tmp = 0;tmp <8;tmp++){
-		if(phase_on_count>val[tmp]) out |= (1<<tmp);
-//		if(phase_on_count>65) out |= (1<<tmp);
-	}
-	out_ext(out);
-
-	if(dmx_lost<DMX_LOST_TIMEOUT){
-		dmx_lost++;
-	}
-}
-
 //############################################################################
 //ISR from external Interrupt (ICP
 ISR (PCINT0_vect){
@@ -149,16 +166,17 @@ void clear_all(){
 }
 
 void debug(void){
+	char temp = 0;
 	clear_all();
-	if(!(PIND&(1<<PD7))) val[7] = 0x0;   //switch 1    PD4   11	 PD7
-	if(!(PIND&(1<<PB1))) val[6] = 0x0;   //switch 2    PD3   13   PB1
-	if(!(PINC&(1<<PC0))) val[5] =0x0;   //switch 3    PC5   23   PC0
-	if(!(PINC&(1<<PC1))) val[4] =0x0;   //switch 4    PC4   24   PC1
-	if(!(PINC&(1<<PC2))) val[3] =0x0;   //switch 5    PC3   25   PC2
-	if(!(PINC&(1<<PC3))) val[2] =0x0;   //switch 6    PC2   26   PC3
-	if(!(PINC&(1<<PC4))) val[1] =0x0;   //switch 7    PC1   27   PC4
-	if(!(PINC&(1<<PC5))) val[0] =0x0;   //switch 8    PC0   28   PC5
-	dmx_lost = 0; //disable check for DMX timeout as not req.
+		if(!(PIND&(1<<PD7))) val[7] = temp;   //switch 1    PD4   11	 PD7
+		if(!(PIND&(1<<PB1))) val[6] = temp;   //switch 2    PD3   13   PB1
+		if(!(PINC&(1<<PC0))) val[5] = temp;   //switch 3    PC5   23   PC0
+		if(!(PINC&(1<<PC1))) val[4] = temp;   //switch 4    PC4   24   PC1
+		if(!(PINC&(1<<PC2))) val[3] = temp;   //switch 5    PC3   25   PC2
+		if(!(PINC&(1<<PC3))) val[2] = temp;   //switch 6    PC2   26   PC3
+		if(!(PINC&(1<<PC4))) val[1] = temp;   //switch 7    PC1   27   PC4
+		if(!(PINC&(1<<PC5))) val[0] = temp;   //switch 8    PC0   28   PC5
+		dmx_lost = 0; //disable check for DMX timeout as not req.
 }
 
 //############################################################################
@@ -196,12 +214,23 @@ int main (void)
 	// :92 - 1 Stepping
 
 	OCR2A = (F_CPU/32/2/50/240) - 1;
+	//Timer for fade up/fade down
+	TCCR1A |= (1<<WGM21);
+	TCCR1B |= (0<<CS21|1<<CS20); // :32
+
+	TIMSK1 |= (1<<OCIE1A);
+
+	// :32 Teiler
+	// :2 Clock Source
+	// :50 Hz
+	// :92 - 1 Stepping
+
+	OCR1A = (F_CPU/32/2/50/240) - 1;
 
 	//Pullup for DIP Switch
 	PORTD |= (1<<PD3)|(1<<PD4)|(1<<PD7);
 	PORTC |= (1<<PC5)|(1<<PC4)|(1<<PC3)|(1<<PC2)|(1<<PC1)|(1<<PC0);
 	PORTB |= (1<<PB1);
-
 	//Infinate loop
 	while(1){
 		//Read DMX Address from Switch
@@ -229,17 +258,14 @@ int main (void)
 		}else{   //debug code goes here
 			debug();
 		}
-
 		if(dmx_lost>=DMX_LOST_TIMEOUT){
 			for(tmp = 0;tmp <8;tmp++){
 				dmx_buffer[dmx_adresse + tmp] = 0;
 			}
 		}
-
 		if((live_counter++) > 1000) {
-			LED_OFF
-//			LED_TOGGLE
-//			live_counter = 0;
+//			LED_OFF
+			live_counter = 0;
 		}
 
 	}
